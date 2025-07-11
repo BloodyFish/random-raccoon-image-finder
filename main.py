244890af90c29222d2ecc742f5d6c7903af02722
BloodyFish <ethan.meyerowitz@gmail.com>
2025-07-10 21:35:13 -0700

    Quick Bug Fix

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from icrawler.builtin import GoogleImageCrawler
 import random 
+import os
 
 
 num_of_raccoons = int(input("How many raccoons would you like: "))
 download_location = input("Download location (use \"/\" ): ")
 
+script_dir = os.path.dirname(__file__)  # directory of the running script
+file_path = os.path.join(script_dir, "english-adjectives.txt")
+
 keywords = []
-with open("RaccoonFinder/english-adjectives.txt", 'r') as file:
+with open(file_path, 'r') as file:
     for line in file:
         keywords.append(line.strip())
 
